465779665a3ae6970b19aec8ca5a9ca9036f213d
2662729661@qq.com <2662729661@qq.com>
2019-03-20 15:09:18 +0800

    ajax

--- src/main/java/com/bigbigmall/xiamen/controller/WelcomeController.java
+++ src/main/java/com/bigbigmall/xiamen/controller/WelcomeController.java
@@ -1,25 +1,8 @@
 package com.bigbigmall.xiamen.controller;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Text;
 
 /**
  * @author lian
@@ -28,187 +11,25 @@ import org.w3c.dom.Text;
 @RequestMapping("/")
 public class WelcomeController {
 
-        /**
-         * https://redan-api.herokuapp.com/profile/personnels
-         *
-         * @param response
-         * @throws Exception
-         */
-        @RequestMapping("/ss")
+        @RequestMapping("/primeNumbers")
         @ResponseBody
-        void get(HttpServletResponse response) throws Exception {
-                //获取文档对象
-                DocumentBuilderFactory newInstance = DocumentBuilderFactory.newInstance();
-                DocumentBuilder newDocumentBuilder = newInstance.newDocumentBuilder();
-                Document doc = newDocumentBuilder.newDocument();
+        public String getPrimeNumbers(int minimum, int maximum) {
+                if (minimum > 0 && (maximum > 0 && maximum > minimum)) {
 
-                //创建根节点
-                Element documentElement = doc.createElement("document");
-                doc.appendChild(documentElement);
-        }
+                        String max = "";
+                        for (int i = minimum; i <= maximum; i++) {
 
-        /**
-         * https://redan-api.herokuapp.com/story/
-         *
-         * @param response
-         * @throws Exception
-         */
-        @RequestMapping("/")
-        @ResponseBody
-        void getStory(HttpServletResponse response) throws Exception {
-                //创建文档对象
-                DocumentBuilderFactory newInstance = DocumentBuilderFactory.newInstance();
-                DocumentBuilder newDocumentBuilder = newInstance.newDocumentBuilder();
-                Document doc = newDocumentBuilder.newDocument();
+                                if (i == 2 || i == 3 || i == 5 || i == 7) {
+                                        max += i + "  ";
+                                }
 
-                //添加根节点
-                Element documentElement = doc.createElement("document");
-                doc.appendChild(documentElement);
-
-                //获取请求体
-                HttpGet httpGet = new HttpGet("https://redan-api.herokuapp.com/story/");
-                CloseableHttpResponse execute = HttpClients.createDefault().execute(httpGet);
-                HttpEntity entity = execute.getEntity();
-
-                if (entity != null) {
-                        String all = EntityUtils.toString(entity, "UTF-8");
-
-                        //添加status属性
-                        String statusValue = new JSONObject(all).get("status").toString();
-                        documentElement.setAttribute("status", statusValue);
-
-                        /*---------------------------获取result的值---------------------------------*/
-                        //获取result值Object
-                        JSONArray resultArray = new JSONObject(all).getJSONArray("result");
-                        JSONObject resultObject = resultArray.getJSONObject(0);
-                        Element storiesElement = doc.createElement("stories");
-                        documentElement.appendChild(storiesElement);
-                        Element storyElement = doc.createElement("story");
-                        storiesElement.appendChild(storyElement);
-
-
-                        /*--------------------------添加节点comments----------------------------------*/
-                        Element commentsElement = doc.createElement("comments");
-                        storyElement.appendChild(commentsElement);
-                        JSONArray commentsArray = resultObject.getJSONArray("comments");
-                        for (int i = 0; i < commentsArray.length(); i++) {
-                                JSONObject commentObject = commentsArray.getJSONObject(i);
-
-                                //创建节点comment
-                                Element commentElement = doc.createElement("comment");
-                                commentsElement.appendChild(commentElement);
-                                //Attribute
-                                commentElement.setAttribute("id", commentObject.get("id").toString());
-                                //Text
-                                Element commentContentElement = doc.createElement("content");
-                                commentElement.appendChild(commentContentElement);
-                                Text commentContentTextNode = doc.createTextNode(commentObject.get("content").toString());
-                                commentContentElement.appendChild(commentContentTextNode);
-                                //who-Attribute
-                                Element commentWhoElement = doc.createElement("who");
-                                commentElement.appendChild(commentWhoElement);
-                                JSONObject whoObject = commentObject.getJSONObject("who");
-                                commentWhoElement.setAttribute("nickname", whoObject.get("nickname").toString());
-                                commentWhoElement.setAttribute("id", whoObject.get("id").toString());
+                                if (!(i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0)) {
+                                        max += i + "  ";
+                                }
                         }
-
-
-                        /*--------------------------添加节点result----------------------------------*/
-                        //Attribute
-                        storyElement.setAttribute("emotions", resultObject.get("emotions").toString());
-                        storyElement.setAttribute("postedAt", resultObject.get("postedAt").toString());
-                        storyElement.setAttribute("id", resultObject.get("id").toString());
-                        //Text
-                        Element contentElement = doc.createElement("content");
-                        storyElement.appendChild(contentElement);
-                        Text contentTextNode = doc.createTextNode(resultObject.get("content").toString());
-                        contentElement.appendChild(contentTextNode);
-                        //author-Attribute
-                        Element authorElement = doc.createElement("author");
-                        storyElement.appendChild(authorElement);
-                        JSONObject authorObject = resultObject.getJSONObject("author");
-                        authorElement.setAttribute("nickname", authorObject.get("nickname").toString());
-                        authorElement.setAttribute("id", authorObject.get("id").toString());
-
+                        return max;
                 }
-
-                //输出到页面
-                TransformerFactory newInstance1 = TransformerFactory.newInstance();
-                Transformer newTransformer = newInstance1.newTransformer();
-                newTransformer.transform(new DOMSource(doc),
-                        new StreamResult(response.getOutputStream()));
-
+                return "Incorrect parameters";
         }
 
-        /**
-         * https://redan-api.herokuapp.com/storyEmotions
-         *
-         * @param response
-         * @throws Exception
-         */
-        @RequestMapping("/storyEmotions")
-        @ResponseBody
-        void getStoryEmotions(HttpServletResponse response) throws Exception {
-                //获取文档对象
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder builder = factory.newDocumentBuilder();
-                Document doc = builder.newDocument();
-
-                //创建根标签
-                Element documentElement = doc.createElement("document");
-                doc.appendChild(documentElement);
-
-                //获取响应数据
-                HttpGet httpget = new HttpGet("https://redan-api.herokuapp.com/storyEmotions");
-                CloseableHttpResponse response1 = HttpClients.createDefault().execute(httpget);
-                HttpEntity entity1 = response1.getEntity();
-
-                if (entity1 != null) {
-
-                        //获取数据对象
-                        String retSrc = EntityUtils.toString(entity1, "UTF-8");
-                        JSONObject all = new JSONObject(retSrc);
-
-                        //第一层-embedded
-                        Element embeddedElement = doc.createElement("_embedded");
-                        documentElement.appendChild(embeddedElement);
-
-                        //第一层-links
-                        Element linksElement = doc.createElement("_links");
-                        documentElement.appendChild(linksElement);
-                        JSONObject linksObject = all.getJSONObject("_links");
-                        //第二层-self
-                        Element selfElement = doc.createElement("self");
-                        linksElement.appendChild(selfElement);
-                        JSONObject selfObject = linksObject.getJSONObject("self");
-                        //第三层-href
-                        Element hrefElement = doc.createElement("href");
-                        selfElement.appendChild(hrefElement);
-                        Text hrefTextNode = doc.createTextNode(selfObject.getString("href"));
-                        hrefElement.appendChild(hrefTextNode);
-                        //第三层-templated
-                        Element templatedElement = doc.createElement("templated");
-                        selfElement.appendChild(templatedElement);
-                        Text templatedTextNode = doc.createTextNode(selfObject.getString("templated"));
-                        hrefElement.appendChild(templatedTextNode);
-
-                        //第一层-page
-                        Element pageElement = doc.createElement("page");
-                        documentElement.appendChild(pageElement);
-                        JSONObject jsonObject = all.getJSONObject("page");
-                        //第二层
-                        Element sizeElement = doc.createElement("size");
-                        pageElement.appendChild(sizeElement);
-                        Element totalElementsElement = doc.createElement("totalElements");
-                        pageElement.appendChild(totalElementsElement);
-                        Element totalPagesElement = doc.createElement("totalPages");
-                        pageElement.appendChild(totalPagesElement);
-                        Element numberElement = doc.createElement("number");
-                        pageElement.appendChild(numberElement);
-
-                }
-                //输出到页面
-                TransformerFactory.newInstance().newTransformer().transform(new DOMSource(doc), new StreamResult(response.getOutputStream()));
-
-        }
 }
